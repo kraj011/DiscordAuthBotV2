24ac17d747d08e410963818657c15d8d24fed623
David Krajewski <davidkrajewski@yahoo.com>
2019-02-26 23:22:35 -0500

    Key activating discord script should be almost (if not already) done. Began also key unbinding from the front end using react. TODO: get json server working or find another way to edit files with react.

--- src/pages/manage.js
+++ src/pages/manage.js
@@ -1,80 +1,96 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import { Table, Divider, Tag, Button } from 'antd';
-import 'antd/dist/antd.css';
-import userInfo from '../info/users.json';
-import Layout from '../components/layout';
+import React from 'react'
+import { Link } from 'gatsby'
+import { Table, Divider, Tag, Button } from 'antd'
+import 'antd/dist/antd.css'
+import userInfo from '../info/users.json'
+import Layout from '../components/layout'
+import fs from 'fs'
 
-const columns = [{
+const columns = [
+  {
     title: 'Key',
-    dataIndex: 'identifier'
-  }, {
+    dataIndex: 'identifier',
+  },
+  {
     title: 'Discord',
     dataIndex: 'discord',
-  }];
+  },
+]
 
 const data = []
 
-for(var i = 0; i < Object.keys(userInfo).length; i++) {
-    data.push({'key': Object.keys(userInfo)[i], 'discord':userInfo[Object.keys(userInfo)[i]] , 'identifier': Object.keys(userInfo)[i]})
+for (var i = 0; i < Object.keys(userInfo).length; i++) {
+  data.push({
+    key: Object.keys(userInfo)[i],
+    discord: userInfo[Object.keys(userInfo)[i]],
+    identifier: Object.keys(userInfo)[i],
+  })
 }
 
 class Manage extends React.Component {
+  state = {
+    selectedRowKeys: [],
+  }
+  selectRow = record => {
+    const selectedRowKeys = [...this.state.selectedRowKeys]
+    if (selectedRowKeys.indexOf(record.key) >= 0) {
+      selectedRowKeys.splice(selectedRowKeys.indexOf(record.key), 1)
+    } else {
+      selectedRowKeys.push(record.key)
+    }
+    this.setState({ selectedRowKeys })
+  }
+  onSelectedRowKeysChange = selectedRowKeys => {
+    this.setState({ selectedRowKeys })
+  }
 
-    state = {
-        selectedRowKeys: [],
-      };
-    selectRow = (record) => {
-        const selectedRowKeys = [...this.state.selectedRowKeys];
-        if (selectedRowKeys.indexOf(record.key) >= 0) {
-          selectedRowKeys.splice(selectedRowKeys.indexOf(record.key), 1);
-        } else {
-          selectedRowKeys.push(record.key);
+  constructor(props) {
+    super(props)
+  }
+
+  unbindKeys = () => {
+    fs.readFile(__dirname + '/../info/users.json', function(err, body, data) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      let json = JSON.parse(data)
+      for (let i = 0; i < this.state['selectedRowKeys'].length; i++) {
+        for (let j = 0; j < Object.keys(json).length; j++) {
+          if (Object.keys(json)[j] == this.state['selectedRowKeys'][i]) {
+            Object.keys(json)[j] = 'none'
+            break
+          }
         }
-        this.setState({ selectedRowKeys });
       }
-    onSelectedRowKeysChange = (selectedRowKeys) => {
-        this.setState({ selectedRowKeys });
-      }
+    })
+  }
 
-      constructor(props) {
-          super(props)
-      }
-
-    unbindKeys = () => {
-        console.log(this.state);
+  render() {
+    const { selectedRowKeys } = this.state
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectedRowKeysChange,
     }
+    // const json = JSON.parse(userInfo);
 
-
-    
-
-    render() {
-        const { selectedRowKeys } = this.state;
-        const rowSelection = {
-        selectedRowKeys,
-        onChange: this.onSelectedRowKeysChange,
-        };
-        // const json = JSON.parse(userInfo);
-        
-        return(
-        <Layout>
-            <h1>Manage Users</h1>
-            <Table
-                rowSelection={rowSelection}
-                columns={columns}
-                dataSource={data}
-                onRow={(record) => ({
-                onClick: () => {
-                    this.selectRow(record);
-                },
-                })}
-            />
-            <Button>Unbind Selected</Button>
-        </Layout>
-        )
-    }
-
-  
+    return (
+      <Layout>
+        <h1>Manage Users</h1>
+        <Table
+          rowSelection={rowSelection}
+          columns={columns}
+          dataSource={data}
+          onRow={record => ({
+            onClick: () => {
+              this.selectRow(record)
+            },
+          })}
+        />
+        <Button onClick={this.unbindKeys}>Unbind Selected</Button>
+      </Layout>
+    )
+  }
 }
 
 export default Manage
--- src/scripts/runDiscord.js
+++ src/scripts/runDiscord.js
@@ -1,9 +1,13 @@
 var Discord = require('discord.js');
 var token = require('../info/settings.json');
 var fs = require('fs');
- 
 
-const { Client, MessageEmbed, DMChannel } = require('discord.js');
+
+const {
+    Client,
+    MessageEmbed,
+    DMChannel
+} = require('discord.js');
 
 // Create an instance of a Discord client
 const client = new Client();
@@ -13,80 +17,94 @@ const client = new Client();
  * received from Discord
  */
 client.on('ready', () => {
-  console.log('I am ready!');
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-  // If the message is "how to embed"
-  if(message.author.bot) return;
+    // If the message is "how to embed"
+    if (message.author.bot) return;
 
-  console.log("got message!")
-  if (message.channel.type == "dm") {
-    var messageSplit = message.content.split(" ");
-    if(messageSplit[0] == "!activate") {
-        // activating key
-        console.log("got activate!")
-       
-        if(messageSplit.length != 2) {
-            // wrong format
-            console.log("wrong format!")
-            return
-        }
-        var key = messageSplit[1]
+    console.log("got message!")
+    if (message.channel.type == "dm") {
+        var messageSplit = message.content.split(" ");
+        if (messageSplit[0] == "!activate") {
+            // activating key
+            console.log("got activate!")
 
-
-        fs.readFile(__dirname + "/../info/users.json", function(err, data) {
-            if(err) {
-                console.log(err)
-            }
-
-            var json = JSON.parse(data);
-            // console.log(json)
-            var keyFound = false
-            for(var i = 0; i < Object.keys(json).length; i++) {
-                var cKey = Object.keys(json)[i]
-                console.log(cKey)
-                if(cKey == key) {
-                    console.log("key found!")
-                    keyFound = true;
-                    if(json[i] == "none") {
-                        // bind discord here
-                    } else {
-                        console.log("key already in use")
-                        const embed = new MessageEmbed()
-                        // Set the title of the field
-                        .setTitle('A slick little embed')
-                        // Set the color of the embed
-                        .setColor(0xFF0000)
-                        // Set the main content of the embed
-                        .setDescription('Hello, this is a slick embed!')
-                        
-                        
-                        message.author.sendEmbed(embed, "TEST!");
-                        return
-                    }
-                }
-            }
-            if(!keyFound) {
-                // key invalid
+            if (messageSplit.length != 2) {
+                // wrong format
+                console.log("wrong format!")
                 return
             }
-
-        })
-
+            var key = messageSplit[1]
 
 
+            fs.readFile(__dirname + "/../info/users.json", function (err, data) {
+                if (err) {
+                    console.log(err)
+                }
 
-    }  else if(message.content.startsWith == "!deactivaate") {
-        // deactivating key
-    }  
-   
-  }
+                var json = JSON.parse(data);
+                // console.log(json)
+                var keyFound = false
+                for (var i = 0; i < Object.keys(json).length; i++) {
+                    var cKey = Object.keys(json)[i]
+                    console.log(cKey)
+                    if (cKey == key) {
+                        console.log("key found!")
+                        keyFound = true;
+
+                        if (json[cKey] == "none") {
+                            // bind discord here
+                            json[cKey] = message.author.tag;
+                            fs.writeFile(__dirname + "/../info/users.json", JSON.stringify(json), function (err) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                            });
+                            message.channel.send({
+                                embed: {
+                                    color: 0x00ff00,
+                                    description: "Activated!"
+                                }
+                            });
+                        } else {
+                            console.log("key already in use")
+
+                            message.channel.send({
+                                embed: {
+                                    color: 0xff0000,
+                                    description: "Key already in use!"
+                                }
+                            });
+                            return
+                        }
+                    }
+                }
+                if (!keyFound) {
+                    // key invalid
+                    return
+                }
+
+            })
+
+
+
+
+        } else if (message.content.startsWith == "!deactivate") {
+            // deactivating key
+        }
+
+    }
 });
 
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
-
-client.login("NTM2NTkxMzQwMzY5MzQ2NTc3.DyY7Jw.vQnK5RkNy3ybD8-G_6pgaBnv94I")
-
-
-
+// Log your bot in using the token from https://discordapp.com/developers/applications/me
+fs.readFile(__dirname + "/../info/settings.json", function (err, data) {
+    if (err) {
+        console.log(err)
+        return;
+    }
+    let json = JSON.parse(data);
+    console.log(json["token"]);
+    client.login(json["token"])
+})
